a6bd3b4645169870e97313ed88fd9ba86bf40720
Lorrain Tapioca <brearey88@yandex.ru>
2025-09-13 22:46:16 +0900

    POST email begin

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
-import express, { Application } from 'express';
-import ticketRouter from './ticket/ticket.router';
-import bodyParser from 'body-parser';
+import express, { Application, Request, Response } from 'express'
+import ticketRouter from './ticket/ticket.router'
+import bodyParser from 'body-parser'
 
-const app: Application = express();
-const PORT = 3002;
+const app: Application = express()
+const PORT = 3002
 
-app.use(bodyParser.json());
-app.use('/api/tickets', ticketRouter);
+app.use(bodyParser.json())
+app.use('/api/tickets', ticketRouter)
+app.post('/api/email', (req: Request, res: Response) => {
+	if (req.body.email) {
+		res.status(200).json({
+			message: 'ok',
+			email: req.body.email,
+		})
+	}
+})
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+async function start() {
+	try {
+		await app.listen(PORT, () => {
+			console.log(`Server running at http://localhost:${PORT}`)
+		})
+	} catch (e) {
+		console.error(e)
+	}
+}
+
+start()
