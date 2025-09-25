c4bdba17438db04c538aa6a2b914478728c3e50a
Lorrain Tapioca <brearey88@yandex.ru>
2025-09-25 21:25:18 +0900

    dotenv added

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express, { Application, Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer'
 // Создаем транспорт для подключения к SMTP Яндекса
 const transporter = nodemailer.createTransport({
 	host: 'smtp.yandex.ru',
-	port: 465,
-	secure: true, // использовать SSL
+	port: 587, // 465 SSL
+	secure: false, // true использовать SSL
 	auth: {
 		user: 'brearey88@yandex.ru',
 		pass: process.env.YANDEX_APP_PASSWORD, // пароль приложения указан на странице Безопасность в id.yandex.ru (название пароля copder)
@@ -13,7 +13,7 @@ const transporter = nodemailer.createTransport({
 
 // Настройки письма
 const mailOptions = {
-	from: '"Ваше имя" ',
+	from: '"Ваше имя" <brearey88@yandex.ru>',
 	to: 'brearey4@gmail.com',
 	subject: 'Тестовое письмо',
 	text: 'Привет! Это простое текстовое письмо.',
@@ -21,14 +21,15 @@ const mailOptions = {
 }
 
 export function sendEmail() {
-	// Отправляем письмо
-	transporter.sendMail(mailOptions, (error, info) => {
-		if (error) {
-			console.log('Ошибка отправки:', error)
-			return error
-		} else {
-			console.log('Письмо отправлено:', info.messageId)
-			return info.messageId
-		}
+	return new Promise((resolve, reject) => {
+		transporter.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				console.log('Ошибка отправки:', error)
+				reject(error)
+			} else {
+				console.log('Письмо отправлено:', info.messageId)
+				resolve(info.messageId)
+			}
+		})
 	})
 }
